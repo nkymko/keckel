12e38b40b1895e7486da1314e1ccdb3c9c36c75d
nkymko <nickymickoayub@gmail.com>
2023-07-05 06:28:03 +0700

    update ui - add footer

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,39 @@
 function showDisplay(displayId, menuId) {
-  let displays = document.getElementsByClassName("display");
-  for (let i = 0; i < displays.length; i++) {
+  const displays = document.getElementsByClassName("display");
+  for (var i = 0; i < displays.length; i++) {
     displays[i].style.display = "none";
   }
 
-  let displayToShow = document.getElementById(displayId);
+  const displayToShow = document.getElementById(displayId);
   displayToShow.style.display = "block";
 
-  let active = document.getElementsByClassName("menu-active");
-  for (let i = 0; i < active.length; i++) {
+  const active = document.getElementsByClassName("menu-active");
+  for (var i = 0; i < active.length; i++) {
     active[i].classList.remove("menu-active");
   }
 
-  let menuToActive = document.getElementById(menuId);
+  const menuToActive = document.getElementById(menuId);
   menuToActive.classList.add("menu-active");
 }
+
+function toggleFixed(fixedId) {
+  const display = document.getElementById('fixed-nav');
+  display.classList.toggle('hidden');
+
+  const displayMenu = document.getElementsByClassName("display-fixed");
+  for (var i = 0; i < displayMenu.length; i++) {
+    displayMenu[i].style.display = 'none';
+  }
+
+  const menuToShow = document.getElementById(fixedId);
+  menuToShow.style.display = 'block';
+}
+
+const fixedMenu = document.querySelector(".fixed-menu");
+
+document.addEventListener("mouseup", function (event) {
+  if (!fixedMenu.contains(event.target)) {
+    const display = document.getElementById('fixed-nav');
+    display.classList.add('hidden');
+  }
+});
